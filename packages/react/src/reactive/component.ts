49716a9a5c2c34a18dd0d7574ed695bbb6f5c42a
shenyunjie <shenyunjie@aidigger.com>
2020-07-13 14:54:16 +0800

    update useContextComponent hook

--- packages/react/src/reactive/component.ts
+++ packages/react/src/reactive/component.ts
@@ -2,14 +2,16 @@ import { ReactApplication } from "../react";
 import { MethodMetaCreator, TypeClientContainer, AnnotationDependenciesAutoRegister, TClassIndefiner, Application } from "@typeclient/core";
 import { NAMESPACE } from "../annotations";
 import { FunctionComponent } from "react";
+import { useApplicationContext } from ".";
 
-export function useContextComponent<Z, T>(app: ReactApplication | Application<any>, server: T, key: keyof T): React.FunctionComponent<Z> {
+export function useContextComponent<Z, T>(server: T, key: keyof T): React.FunctionComponent<Z> {
   if (server.constructor && server.constructor.prototype && server[key] && typeof server[key] === 'function') {
     const instance = MethodMetaCreator.instance(Object.getOwnPropertyDescriptor(server.constructor.prototype, key));
     // AnnotationDependenciesAutoRegister(server.constructor as TClassIndefiner<any>, TypeClientContainer);
     const isComponent = instance.got(NAMESPACE.COMPONENT, false);
     if (isComponent) {
-      const fcs = (app as ReactApplication).FCS;
+      const ctx = useApplicationContext();
+      const fcs = (ctx.app as ReactApplication).FCS;
       if (!fcs.has(server)) fcs.set(server, new Map());
       const target = fcs.get(server);
       if (!target.has(key as string)) {
