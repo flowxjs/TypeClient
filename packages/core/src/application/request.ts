419977a24980ad6e116aa5055ab9cd1133115038
shenyunjie <shenyunjie@aidigger.com>
2020-07-21 15:29:58 +0800

    remove request.referer

--- packages/core/src/application/request.ts
+++ packages/core/src/application/request.ts
@@ -4,7 +4,6 @@ export type TRequestParams = { [key: string]: string };
 
 export class Request extends Url {
   public params: TRequestParams = {};
-  public referer: string | null;
   // request method using 'GET' by default
   public readonly method = 'GET';
 
