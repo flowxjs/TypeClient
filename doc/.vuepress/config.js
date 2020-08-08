93622c1db7ab08117497d9e5890291d781a02e8f
shenyunjie <shenyunjie@aidigger.com>
2020-08-08 16:27:23 +0800

    update

---
+++ doc/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'Type Client Framework',
+  description: 'Just playing around',
+  base: '/docs',
+  themeConfig: {
+    sidebar: 'auto',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '介绍', link: '/zh/guide' },
+      { text: '核心', link: '/zh/core' },
+      { text: 'React', link: '/zh/core' },
+    ]
+  }
+}
